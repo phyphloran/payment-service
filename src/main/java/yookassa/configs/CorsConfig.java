851ab88b50e029a4d93020470efb1a796f2ccfd8
phyphloran <mareeviv@yandex.ru>
2026-02-09 23:47:44 +0300

    added actuator, prometheus to pom.xml

---
+++ src/main/java/yookassa/configs/CorsConfig.java
@@ -0,0 +1,26 @@
+package yookassa.configs;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://127.0.0.1:*",
+                        "http://45.8.249.247"
+                )
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
